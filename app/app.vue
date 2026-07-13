b82322674f328630d34ee25a7501754852af95a4
Yo-code_back <biancheng453@gmail.com>
2026-07-13 19:08:02 +0800

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -33,7 +33,7 @@
         <div class="status-label">
           <span>最新狀態</span>
           <span class="status-label__rule" aria-hidden="true" />
-          <span>系統測試 (07/13)</span>
+          <span>修復軟體 預估1~2小時開放(07/13)</span>
         </div>
 
         <h1 id="maintenance-title" class="maintenance-title">
